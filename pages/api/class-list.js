108e7ef11444fb02627aef4595eea3692f5e4925
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-09-12 10:27:29 +0700

    Delete pages/api directory

--- pages/api/class-list.js
+++
@@ -1,6 +0,0 @@
-import { getClassList } from "../../lib/sheetService";
-
-export default async function handler(req, res) {
-  const classes = await getClassList();
-  return res.status(200).json(classes);
-}
